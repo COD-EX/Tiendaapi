d6132950ed370873d9301ab4b7e5c9a6b8fcd5b6
Alin Piero <alinpiero@gmail.com>
2023-02-24 18:32:34 -0500

    Add project files.

---
+++ Conexion/ConexionDB.cs
@@ -0,0 +1,22 @@
+﻿namespace Tiendaapi.Conexion
+{
+    public class ConexionDB
+    {
+        //creación de variable llamado connectionstring como vacío
+        private string connectionString = string.Empty;
+
+        //constructor ConexionDB
+        public ConexionDB()
+        {
+            //creación de variable encapsulada
+            var constructor = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile ("appsettings.json").Build();
+            connectionString = constructor.GetSection("ConnectionStrings:conexionmaestra").Value;
+        }
+        //Ahora para que nuestra conexión sea reconocido desde cualquier parte de nuestro proyecto o como público escribimos lo siguiente
+        public string cadenaSQL()//un nombre del método cualquiera
+        {
+            return connectionString; //retorna la conexión
+        }
+    }
+}
---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiendaapi.Datos;
+using Tiendaapi.Modelo;
+
+namespace Tiendaapi.Controllers
+{
+
+    //haciendo primero la referencia
+    [ApiController]
+
+    //Rutas
+    [Route("api/prueba")]
+    public class ProductosController
+    {
+        //nuestro primer proceso
+
+        [HttpGet]
+        public async Task<ActionResult<List<MProductos>>> Get()
+        {
+            var funcion = new DProductos();
+            var lista = await funcion.Mostrarproductos();
+            return lista;
+        } 
+    }
+}
